25f4ec2084fb3e1c9246b2733bbf68e4d592cf5b
nppnam05 <nppnam05@gmail.com>
2025-10-13 23:42:58 +0700

    config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -7,32 +7,14 @@ import 'firebase_options.dart';
 
 void main() async {
   //set
+  // Khởi tạo binding trước
   WidgetsFlutterBinding.ensureInitialized();
   configureInjectable();
 
+  // init firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
-}
-
-void configureFirebase() {
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  // Dữ liệu test xem có kết nối database chưa
-  //   await FirebaseFirestore.instance.collection("users").add({
-  //   "name": "JonDang",
-  //   "email": "JonDang@example.com",
-  //   "phone": "+84987654321",
-  //   "urlAvatar": "https://example.com/avatar_jon.png",
-  //   "address": {
-  //     "street": "456 Le Loi",
-  //     "city": "Da Nang",
-  //     "country": "Việt Nam",
-  //   },
-  //   "createdAt": DateTime.now().toIso8601String(),
-  //   "stats": {
-  //     "completedTours": 3,
-  //     "favoriteCount": 5,
-  //     "totalBookings": 4,
-  //     "totalSpent": 1599.50,
-  //   }
-  // });
 }
