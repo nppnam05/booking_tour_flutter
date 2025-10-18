1dc32809ab552cbc6ad423a303eb795c76fbbd5b
Nhatt0 <150875657+Nhatt0@users.noreply.github.com>
2025-10-18 21:59:53 +0700

    Refactor : Managerment trip screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:booking_tour_flutter/app/dependency_injection/configure_injectable.dart';
-import 'package:booking_tour_flutter/presentation/widgets_dialog/test_dialog_screen.dart';
+import 'package:booking_tour_flutter/presentation/trip/trip_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,9 +22,9 @@ class _App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Dialog Test',
+      title: 'Booking Tour Flutter',
       theme: ThemeData(useMaterial3: true),
-      home: const TestDialogScreen(),
+      home: const TripScreen(),
     );
   }
 }
