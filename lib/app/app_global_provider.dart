5c9ef29e9c007cd387a5880fa4ff559e8565c111
nppnam05 <nppnam05@gmail.com>
2025-10-28 19:51:48 +0700

    update assignment screen

--- lib/app/app_global_provider.dart
+++ lib/app/app_global_provider.dart
@@ -1,4 +1,5 @@
-import 'package:booking_tour_flutter/presentation/assignment/tour_guide_assignment/cubit/tour_guide_assignment_cubit.dart';
+import 'package:booking_tour_flutter/presentation/tour_manager/assignment/schedule_assignment/cubit/schedule_assignment_cubit.dart';
+import 'package:booking_tour_flutter/presentation/tour_manager/assignment/tour_guide_assignment/cubit/tour_guide_assignment_cubit.dart';
 import 'package:booking_tour_flutter/presentation/tour_manager/add_tour/cubit/add_tour_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,6 +14,7 @@ class AppGlobalProvider extends StatelessWidget{
     return MultiBlocProvider(providers: [
       BlocProvider(create: (_) => AddTourCubit()),
       BlocProvider(create: (_) => TourGuideAssignmentCubit()),
+      BlocProvider(create: (_) => ScheduleAssignmentCubit()),
     ], child: child);
   }
 
