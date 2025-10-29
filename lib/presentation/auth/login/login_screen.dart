b02c8944c192b85a90b18fa365569b1623b18dff
nppnam05 <nppnam05@gmail.com>
2025-10-29 13:03:28 +0700

    update login

--- lib/presentation/auth/login/login_screen.dart
+++ lib/presentation/auth/login/login_screen.dart
@@ -54,7 +54,7 @@ class LoginScreen extends StatelessWidget {
             case 2:
               Navigator.pushNamedAndRemoveUntil(
                 context,
-                RouteName.tripList,
+                RouteName.scheduleTourguide,
                 (route) => false,
               );
               return;
