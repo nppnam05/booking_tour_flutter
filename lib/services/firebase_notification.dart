0302c1cb6244675e51d55730b79935bfb681e43a
nppnam05 <nppnam05@gmail.com>
2025-11-23 03:58:06 +0700

    done notification

---
+++ lib/services/firebase_notification.dart
@@ -0,0 +1,62 @@
+import 'package:booking_tour_flutter/app/app_navigator.dart';
+import 'package:booking_tour_flutter/app/route_manager.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+class FirebaseNotification {
+  FirebaseNotification._();
+
+  static Future<void> initFCM() async {
+    final messaging = FirebaseMessaging.instance;
+
+    await messaging.requestPermission();
+    // String? token = await messaging.getToken();
+    
+    FirebaseMessaging.onMessage.listen((message) {
+      showNotification(
+        message.notification?.title ?? 'Thông báo',
+        message.notification?.body ?? '',
+      );
+    });
+  }
+
+  static Future<void> showNotification(String title, String body) async {
+    await flutterLocalNotificationsPlugin.show(
+      DateTime.now().millisecond,
+      title,
+      body,
+      const NotificationDetails(
+        android: AndroidNotificationDetails(
+          'high_importance_channel',
+          'Default',
+          importance: Importance.high,
+        ),
+      ),
+    );
+  }
+
+  static Future<void> initNotifications() async {
+    await flutterLocalNotificationsPlugin.initialize(
+      const InitializationSettings(
+        android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+      ),
+    );
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin
+        >()
+        ?.createNotificationChannel(
+          const AndroidNotificationChannel(
+            'high_importance_channel',
+            'Default',
+            importance: Importance.high,
+          ),
+        );
+  }
+}
