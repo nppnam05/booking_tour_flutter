beeb97c74b825907292b6d50c1fbdd0e8d9713a4
the3t <thanhthuong12032005@gmail.com>
2025-11-24 21:49:43 +0700

    fix schedule tourguide cubit can't init

--- lib/presentation/tour_guide/cubit/schedule_tourguide_cubit.dart
+++ lib/presentation/tour_guide/cubit/schedule_tourguide_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:booking_tour_flutter/app/app_navigator.dart';
 import 'package:booking_tour_flutter/app/dependency_injection/configure_injectable.dart';
 import 'package:booking_tour_flutter/data/booking_repository.dart';
 import 'package:booking_tour_flutter/presentation/auth/auth_cubit.dart';
@@ -6,23 +7,20 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ScheduleTourguideCubit extends Cubit<ScheduleTourguideState> {
   final BookingRepository bookingRepository = getIt<BookingRepository>();
-  final AuthCubit authCubit = getIt<AuthCubit>();
 
   ScheduleTourguideCubit() : super(ScheduleTourguideInitial());
 
   Future<void> loadSchedules() async {
     emit(ScheduleTourguideLoading());
 
-    final user = authCubit.state.id;
-    if (user == null) {
+    final userId = AppNavigator.currentContext.read<AuthCubit>().state.id;
+    if (userId == null) {
       emit(ScheduleTourguideError("Không tìm thấy thông tin đăng nhập"));
       return;
     }
 
-    final staffId = authCubit.state.id;
-
     final result =
-        await bookingRepository.getSchedulesByStaff(staffId: staffId);
+        await bookingRepository.getSchedulesByStaff(staffId: userId);
 
     result.fold(
       (failure) => emit(ScheduleTourguideError(failure.message)),
