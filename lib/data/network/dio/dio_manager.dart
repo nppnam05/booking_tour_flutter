e14043060b8e5c8069ac0667039295babaefb74b
the3t <thanhthuong12032005@gmail.com>
2025-11-24 16:34:31 +0700

    staging

--- lib/data/network/dio/dio_manager.dart
+++ lib/data/network/dio/dio_manager.dart
@@ -9,10 +9,10 @@ class HTTPHeader {
 
 class HTTPValue {
   static final String contentJson = "application/json";
-  static final String baseURL = "http://192.168.0.54:8080/";
+  // static final String baseURL = "http://192.168.0.54:8080/";
   // static final String baseURL = "http://192.168.16.89:8080/";
 
-  // static final String baseURL = "http://tt1220-001-site1.ntempurl.com";
+  static final String baseURL = "http://tt1220-001-site1.ntempurl.com";
 }
 
 @module
